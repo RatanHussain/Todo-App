2cd0a61b303acdec772540c60cdea9224207d8b6
Ratan Hussain <mdratanmia7395@gmail.com>
2023-11-27 13:26:37 +0300

    add classes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // Select all element here
 let inputTodo = document.querySelector('#inputTodo');
 let todoButton = document.querySelector('#todoButton');
@@ -6,74 +8,79 @@ let todoLists = document.querySelector('#todoLists');
 let todoList = document.querySelector('li');
 let message = document.querySelector('p');
 
+// todo make here by classes
 
-
-const deleteTodo = (id)=>{
-    id.style.display = none;
+class makeTodo {
+	constructor(todoId, todoValue) {
+		this.todoId = todoId;
+		this.todoValue = todoValue;
+	}
 }
 
-const createTodo = (todoId , todoValue)=>{
-    const todoElement = document.createElement('li');
-    todoElement.id = todoId;
-    todoElement.innerHTML = `<span>${todoValue}</span> <span><button id="deleteBtn" ><i class="fa-solid fa-trash"></i></button></span>`
-    todoLists.appendChild(todoElement);
+const deleteTodo = (id) => {
+	id.style.display = none;
+};
 
-    let deltodo = todoElement.querySelector('#deleteBtn');
-    deltodo.addEventListener('click',(event)=>{
-        let deltag = event.target.parentElement.parentElement.parentElement
-        todoLists.removeChild(deltag);
-        messagefn('Todo is Deleted.', 'message1');
-        let todos = getTodosFromLocalStorage();
-        todos = todos.filter((todo) => todo.id !== deltag.id);
-        localStorage.setItem('mytodos', JSON.stringify(todos));
+const createTodo = (newtodos) => {
+	const todoElement = document.createElement('li');
+	todoElement.id = newtodos.todoId;
+	todoElement.innerHTML = `<span>${newtodos.todoValue}</span> <span><button id="deleteBtn" ><i class="fa-solid fa-trash"></i></button></span>`;
+	todoLists.appendChild(todoElement);
 
-    })
-}
+	let deltodo = todoElement.querySelector('#deleteBtn');
+	deltodo.addEventListener('click', (event) => {
+		let deltag = event.target.parentElement.parentElement.parentElement;
+		todoLists.removeChild(deltag);
+		messagefn('Todo is Deleted.', 'message1');
+		let todos = getTodosFromLocalStorage();
+		todos = todos.filter((todo) => todo.todoId !== deltag.id);
+		localStorage.setItem('mytodos', JSON.stringify(todos));
+	});
+};
 
-let messagefn = (sms , id)=>{
-    message.innerHTML = sms;
-    message.id = id
-    setTimeout(() => {
-        message.innerHTML = ''
-        message.id = ''
-    }, 1000);
-}
+let messagefn = (sms, id) => {
+	message.innerHTML = sms;
+	message.id = id;
+	setTimeout(() => {
+		message.innerHTML = '';
+		message.id = '';
+	}, 1000);
+};
 
 // getT odos From LocalStorage
-const getTodosFromLocalStorage = ()=>{
-    return localStorage.getItem('mytodos') ? JSON.parse(localStorage.getItem('mytodos')) : [];
-}
-
+const getTodosFromLocalStorage = () => {
+	return localStorage.getItem('mytodos')
+		? JSON.parse(localStorage.getItem('mytodos'))
+		: [];
+};
 
 // add Todo
-const addTodo = (event)=>{
-    event.preventDefault();
-    if(inputTodo.value === ''){
-        messagefn('You must enter a todo.','message1')
-    }else{
-        let todovalue = inputTodo.value;
-    // Generate unique id
-    const id = Date.now().toString();
-    createTodo(id , todovalue);
-    messagefn('Todo is Created','message');
+const addTodo = (event) => {
+	event.preventDefault();
+	if (inputTodo.value === '') {
+		messagefn('You must enter a todo.', 'message1');
+	} else {
+		let todovalue = inputTodo.value;
+		// Generate unique id
+		const id = Date.now().toString();
+        let newtodos = new makeTodo(id,todovalue)
+		createTodo(newtodos);
+		messagefn('Todo is Created', 'message');
 
+        // adding toto in localStoeage
+		const todos = getTodosFromLocalStorage();
+		todos.push(newtodos);
+		localStorage.setItem('mytodos', JSON.stringify(todos));
 
-    // adding toto in localStoeage
-    const todos = getTodosFromLocalStorage();
-    todos.push({id,todovalue});
-    localStorage.setItem('mytodos', JSON.stringify(todos));
+		inputTodo.value = '';
+	}
+};
 
-    inputTodo.value = '';
-    }
-
-}
-
-const loadtodo = ()=>{
-    let todos = getTodosFromLocalStorage();
-    todos.map((todo)=> createTodo(todo.id , todo.todovalue));
-
-}
+const loadtodo = () => {
+	let todos = getTodosFromLocalStorage();
+	todos.map((todo) => createTodo(todo));
+};
 
 // listener here
-todoForm.addEventListener('submit',addTodo)
-window.addEventListener('DOMContentLoaded',loadtodo)
+todoForm.addEventListener('submit', addTodo);
+window.addEventListener('DOMContentLoaded', loadtodo);
